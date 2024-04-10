8992a2e73180040415d4758641ab569403cba7ac
hawardShin <joushin@student.42seoul.kr>
2024-04-10 19:31:45 +0900

    Fix: CorsError Fix

--- src/main/java/capstone/relation/websocket/WebSocketConfig.java
+++ src/main/java/capstone/relation/websocket/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws-chat").withSockJS();
+		registry.addEndpoint("/ws-chat").setAllowedOriginPatterns("*").withSockJS();
 	}
 }
