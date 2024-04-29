a6ecbad6fefec190675ce79f06d786b12755d7ec
hawardShin <joushin@student.42seoul.kr>
2024-04-29 15:00:52 +0900

    KAN-41 : Feat: DatabaseInitializer with All School data API

---
+++ src/main/java/capstone/relation/workspace/school/CustomInitListener.java
@@ -0,0 +1,19 @@
+package capstone.relation.workspace.school;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import capstone.relation.workspace.school.service.SchoolService;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CustomInitListener {
+
+	private final SchoolService schoolService;
+
+	@EventListener
+	public void onCustomInit(CustomInitEvent event) {
+		schoolService.initSchool();
+	}
+}
