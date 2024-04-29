a96968e69b6e5d4ec482ffd9ed3517ef793f0695
hawardShin <joushin@student.42seoul.kr>
2024-04-29 14:44:58 +0900

    KAN-41 : Feat: School Api Response Dto Set

---
+++ src/main/java/capstone/relation/workspace/school/dto/response/SchoolInfo.java
@@ -0,0 +1,18 @@
+package capstone.relation.workspace.school.dto.response;
+
+import lombok.Data;
+
+@Data
+public class SchoolInfo {
+	private String campusName;
+	private String collegeinfourl;
+	private String schoolType;
+	private String link;
+	private String schoolGubun;
+	private String adres;
+	private String schoolName;
+	private String region;
+	private String totalCount;
+	private String estType;
+	private String seq;
+}
