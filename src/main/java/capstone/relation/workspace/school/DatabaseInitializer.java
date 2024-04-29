27811eb9fe772ebbb0426bc42c80586d2ba932c7
hawardShin <joushin@student.42seoul.kr>
2024-04-29 15:00:39 +0900

    KAN-41 : Feat: DatabaseInitializer with All School data API

---
+++ src/main/java/capstone/relation/workspace/school/DatabaseInitializer.java
@@ -0,0 +1,21 @@
+package capstone.relation.workspace.school;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class DatabaseInitializer implements ApplicationListener<ContextRefreshedEvent> {
+	private final ApplicationEventPublisher eventPublisher;
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		// 이벤트 발행
+		eventPublisher.publishEvent(new CustomInitEvent(this));
+	}
+}
+
