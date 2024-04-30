f03936410903653fd3cc6a1ed7defac713346a1e
hawardShin <joushin@student.42seoul.kr>
2024-05-01 00:35:56 +0900

    KAN-41 : Feat: WebSocketConfig with preHandler Set to Session And ErrorHandler

--- src/main/java/capstone/relation/websocket/WebSocketConfig.java
+++ src/main/java/capstone/relation/websocket/WebSocketConfig.java
@@ -1,14 +1,21 @@
 package capstone.relation.websocket;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+	private final StompPreHandler stompPreHandler;
+	private final StompErrorHandler stompErrorHandler;
+
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
 		// 메시지를 구독하는 요청 url
@@ -18,6 +25,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws-chat").setAllowedOriginPatterns("*").withSockJS();
+		registry.setErrorHandler(stompErrorHandler).addEndpoint("/ws-chat").setAllowedOriginPatterns("*").withSockJS();
+	}
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(stompPreHandler);
 	}
 }
