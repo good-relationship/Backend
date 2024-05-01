4eced9afeb6a0f0dd39f5e31b476825a4f2031a2
hawardShin <joushin@student.42seoul.kr>
2024-05-01 16:08:38 +0900

    KAN-41 : Feat: get 10OrderById

--- src/main/java/capstone/relation/websocket/chat/repository/ChatRepository.java
+++ src/main/java/capstone/relation/websocket/chat/repository/ChatRepository.java
@@ -1,11 +1,14 @@
 package capstone.relation.websocket.chat.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import capstone.relation.websocket.chat.domain.Chat;
+import capstone.relation.workspace.WorkSpace;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-	
+	List<Chat> findTop10ByWorkSpaceAndIdLessThanOrderByIdDesc(WorkSpace workSpace, Long lastMsgId);
 }
