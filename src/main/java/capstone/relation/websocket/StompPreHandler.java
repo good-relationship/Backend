c7a9054cc4e6f8b4ec20efdf20167c256dd97d34
hawardShin <joushin@student.42seoul.kr>
2024-05-01 16:06:10 +0900

    KAN-41 : Feat: JWT Session Expire Time clear

--- src/main/java/capstone/relation/websocket/StompPreHandler.java
+++ src/main/java/capstone/relation/websocket/StompPreHandler.java
@@ -1,7 +1,6 @@
 package capstone.relation.websocket;
 
-import java.util.Map;
-import java.util.Objects;
+import java.time.Instant;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
@@ -9,54 +8,54 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.security.core.Authentication;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import capstone.relation.api.auth.exception.AuthErrorCode;
 import capstone.relation.api.auth.exception.AuthException;
 import capstone.relation.api.auth.jwt.TokenProvider;
-import capstone.relation.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@RequiredArgsConstructor
 @Configuration
 @Slf4j
 public class StompPreHandler implements ChannelInterceptor {
 	private final TokenProvider tokenProvider;
-	private final UserRepository userRepository;
+	private final ThreadPoolTaskScheduler taskScheduler;
+
+	StompPreHandler(TokenProvider tokenProvider) {
+		this.tokenProvider = tokenProvider;
+		taskScheduler = new ThreadPoolTaskScheduler();
+		taskScheduler.initialize();
+	}
 
 	@Override
 	public Message<?> preSend(Message<?> message, MessageChannel channel) {
 		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 		if (accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
-			String authToken = accessor.getFirstNativeHeader("Authorization");
-			if (authToken == null) {
+			System.out.println("preSend CONNECT가 실행");
+			String token = accessor.getFirstNativeHeader("Authorization");
+			System.out.println("token: " + token);
+			if (token == null) {
 				throw new AuthException(AuthErrorCode.INVALID_ACCESS_TOKEN);
 			}
-			Authentication authentication = tokenProvider.getAuthentication(authToken);
-			if (authentication != null) {
-				setUserDetails(accessor, authentication);
-				return message;
+			if (!tokenProvider.validateToken(token)) {
+				throw new AuthException(AuthErrorCode.INVALID_ACCESS_TOKEN);
 			}
-			throw new AuthException(AuthErrorCode.INVALID_ACCESS_TOKEN);
+			Long userId = tokenProvider.getUserId(token);
+			Long expiryTime = tokenProvider.getExpiryFromToken(token); //나중에 만료시간 설정해서 스캐줄러에 넣으려고
+			accessor.getSessionAttributes().put("userId", userId);
+			accessor.getSessionAttributes().put("expiryTime", expiryTime);
+			scheduleSessionExpiry(accessor, expiryTime);
 		}
 		return message;
 	}
 
-	private void setUserDetails(StompHeaderAccessor accessor, Authentication authentication) {
-		Long userId = (Long)authentication.getPrincipal();
-		String userName = userRepository.findById(userId).get().getUserName();
-		setSessionAttributes(accessor, userId, userName);
-		log.info("Session attributes after setting: " + Objects.requireNonNull(accessor.getSessionAttributes())
-			.toString());
-	}
-
-	private void setSessionAttributes(StompHeaderAccessor accessor, Long userId,
-		String userName) {
-		Map<String, Object> sessionAttributes = Objects.requireNonNull(accessor.getSessionAttributes());
-		sessionAttributes.put("userName", userName);
-		sessionAttributes.put("userId", userId);
-		accessor.setSessionAttributes(sessionAttributes);
+	private void scheduleSessionExpiry(StompHeaderAccessor accessor, Long expiryTime) {
+		Instant expireAt = Instant.ofEpochMilli(expiryTime); // 밀리초 단위의 타임스탬프를 Instant 객체로 변환
+		System.out.println("Session expiry scheduled for sessionId: " + accessor + " at: " + expireAt);
+		taskScheduler.schedule(() -> {
+			accessor.getSessionAttributes().clear();
+			System.out.println("Session expired and automatically closed for sessionId: 세션 만료!!");
+		}, expireAt);
 	}
 }
 
