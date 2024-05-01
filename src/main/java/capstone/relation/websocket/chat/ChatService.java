ef81044ac19610747e94e0fbbe19f3243e3c2c61
hawardShin <joushin@student.42seoul.kr>
2024-05-01 16:07:42 +0900

    KAN-41 : Feat: GetHistory Service And Throw Exception Make in SendMessage

--- src/main/java/capstone/relation/websocket/chat/ChatService.java
+++ src/main/java/capstone/relation/websocket/chat/ChatService.java
@@ -1,12 +1,15 @@
 package capstone.relation.websocket.chat;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import capstone.relation.api.auth.exception.AuthErrorCode;
+import capstone.relation.api.auth.exception.AuthException;
 import capstone.relation.user.domain.User;
 import capstone.relation.user.repository.UserRepository;
 import capstone.relation.websocket.chat.domain.Chat;
@@ -31,17 +34,30 @@ public class ChatService {
 			return null;
 		Map<String, Object> sessionAttributes = headerAccessor.getSessionAttributes();
 		Long userId = (Long)sessionAttributes.get("userId");
-		String userName = (String)sessionAttributes.get("userName");
-		if (userId == null || userName == null)
-			return null;
+		if (userId == null) {
+			throw new AuthException(AuthErrorCode.INVALID_ACCESS_TOKEN);
+		}
 		User user = userRepository.findById(userId).orElse(null);
 		WorkSpace workSpace = workSpaceRepository.findById(workSpaceId).orElse(null);
 		if (user == null || workSpace == null || user.getWorkSpace() != workSpace) {
-			return null;
+			throw new AuthException(AuthErrorCode.INVALID_ACCESS_TOKEN);
 		}
 		Chat chat = new Chat(user, workSpace, content, LocalDateTime.now());
 		chatRepository.save(chat);
 		return ChatMapper.INSTANCE.chatToMessageDto(chat);
+	}
+
+	//TODO : Exception 클래스 만들어 처리
+	public List<MessageDto> getHistory(Long lastMsgId, SimpMessageHeaderAccessor headerAccessor) {
+		Map<String, Object> sessionAttributes = headerAccessor.getSessionAttributes();
+		Long userId = (Long)sessionAttributes.get("userId");
+		User user = userRepository.findById(userId).orElse(null);
+		WorkSpace workSpace = user.getWorkSpace();
+		if (workSpace == null)
+			return null;
+		List<Chat> chats = chatRepository.findTop10ByWorkSpaceAndIdLessThanOrderByIdDesc(workSpace,
+			lastMsgId);
+		return ChatMapper.INSTANCE.chatToMessageDtoList(chats);
 	}
 }
 
