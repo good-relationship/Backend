299645ed0a9f1714ecfe609382091b71ebe0ce68
hawardShin <joushin@student.42seoul.kr>
2024-05-01 16:09:20 +0900

    KAN-41 : Feat: Chat History And new Chat Error handling

--- src/main/java/capstone/relation/websocket/chat/controller/ChatController.java
+++ src/main/java/capstone/relation/websocket/chat/controller/ChatController.java
@@ -1,8 +1,8 @@
 package capstone.relation.websocket.chat.controller;
 
+import java.security.Principal;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -11,12 +11,11 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import capstone.relation.api.auth.exception.AuthException;
 import capstone.relation.websocket.chat.ChatService;
 import capstone.relation.websocket.chat.dto.publish.HistoryPublishDto;
 import capstone.relation.websocket.chat.dto.publish.MessagePublishDto;
-import capstone.relation.websocket.chat.dto.response.HistoryResponseDto;
 import capstone.relation.websocket.chat.dto.response.MessageDto;
-import capstone.relation.websocket.chat.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -24,34 +23,32 @@ import lombok.RequiredArgsConstructor;
 public class ChatController {
 	private final ChatService chatService;
 	private final SimpMessagingTemplate simpMessagingTemplate;
-	MessageRepository messageRepository = new MessageRepository();
 
 	@MessageMapping("/message/{workSpaceId}")
 	@SendTo("/topic/message/{workSpaceId}")
 	public ResponseEntity<MessageDto> newMessage(@DestinationVariable String workSpaceId, MessagePublishDto message,
 		SimpMessageHeaderAccessor headerAccessor) throws Exception { //TODO: 토큰 만료 되는 경우 어떻게 함?
-		MessageDto messageDto = chatService.sendNewMessage(workSpaceId, message.getContent(), headerAccessor);
-		//TODO: NULL 처리
-		System.out.println("Send message: " + messageDto);
-		return ResponseEntity.ok(messageDto);
+		try {
+			MessageDto messageDto = chatService.sendNewMessage(workSpaceId, message.getContent(), headerAccessor);
+			System.out.println("Send message: " + messageDto);
+			if (messageDto == null) {
+				return ResponseEntity.badRequest().build();
+			}
+			return ResponseEntity.ok(messageDto);
+		} catch (AuthException e) {
+			return ResponseEntity.status(401).build();
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
 	}
 
-	//TODO : 아직 안 했음 해야함.
-	@MessageMapping("/history/{workSpaceId}")
-	@SendTo("/topic/history/{worksSpaceId}")
-	public ResponseEntity<HistoryResponseDto> history(@DestinationVariable String workSpaceId, HistoryPublishDto join,
+	@MessageMapping("/history")
+	public void history(Principal principal, HistoryPublishDto historyPublishDto,
 		SimpMessageHeaderAccessor headerAccessor) throws
 		Exception {
-		HistoryResponseDto messages = new HistoryResponseDto();
-		System.out.println("Received history: " + join.toString());
-		List<MessageDto> mess = MessageRepository.getMessages(join.getRoomId());
-		if (mess == null) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-		}
-		messages.setMessages(mess);
-		messages.setEnd(false);
-		messages.setLastMsgId(mess.get(mess.size() - 1).getMessageId());
-		System.out.println("반환값:" + messages);
-		return ResponseEntity.ok(messages);
+		List<MessageDto> messages = chatService.getHistory(historyPublishDto.getLastMsgId(), headerAccessor);
+		System.out.println("Send history: " + messages);
+		System.out.println("Principal: " + principal.getName());
+		simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/topic/history", ResponseEntity.ok(messages));
 	}
 }
