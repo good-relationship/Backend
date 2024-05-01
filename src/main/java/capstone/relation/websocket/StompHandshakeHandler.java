d7e8c201000f822771a30f2a4d37754670467950
hawardShin <joushin@student.42seoul.kr>
2024-05-01 16:05:11 +0900

    KAN-41 : Feat: StompPrincipal For Target Send Make Id UUID in HandShake
    - 이거 잘 봐야 한다. STOMP CONNECTION 시점이 아님.

---
+++ src/main/java/capstone/relation/websocket/StompHandshakeHandler.java
@@ -0,0 +1,25 @@
+package capstone.relation.websocket;
+
+import java.security.Principal;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class StompHandshakeHandler extends DefaultHandshakeHandler {
+
+	@Override
+	protected Principal determineUser(ServerHttpRequest request,
+		WebSocketHandler wsHandler,
+		Map<String, Object> attributes) {
+		System.out.println("HANDSHAKE");
+		return new StompPrincipal(UUID.randomUUID().toString());
+	}
+}
