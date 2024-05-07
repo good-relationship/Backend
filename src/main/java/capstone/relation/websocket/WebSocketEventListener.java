4a1014ebe50cb3771e42737bd56d60131fc9ecce
hawardShin <joushin@student.42seoul.kr>
2024-05-07 17:49:46 +0900

    KAN-64 : Feat: SocketEventListener With Disconnection Delete socketRegistry

---
+++ src/main/java/capstone/relation/websocket/WebSocketEventListener.java
@@ -0,0 +1,28 @@
+package capstone.relation.websocket;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+
+	private final SocketRegistry socketRegistry;
+
+	@EventListener
+	public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+		// String sessionId = headerAccessor.getSessionId();
+		String socketId = headerAccessor.getUser().getName();
+		Long userId = (Long)headerAccessor.getSessionAttributes().get("userId");
+		System.out.println("User Disconnected : " + userId);
+		System.out.println("socketId : " + socketId);
+		System.out.println("register SocketId" + socketRegistry.getSocketId(userId.toString()));
+		if (socketId == socketRegistry.getSocketId(userId.toString()))
+			socketRegistry.unregisterSession(userId.toString());
+	}
+}
