4bd5dd393484bad60c646163e39816cf83dcedf1
hawardShin <joushin@student.42seoul.kr>
2024-05-08 17:17:11 +0900

    KAN-44: Docs: Set Dto

---
+++ src/main/java/capstone/relation/websocket/meeting/dto/JoinDto.java
@@ -0,0 +1,19 @@
+package capstone.relation.websocket.meeting.dto;
+
+import java.util.List;
+
+import capstone.relation.user.dto.UserInfoDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class JoinDto {
+	@Schema(description = "회의실 ID", example = "123")
+	private String roomId;
+	@Schema(description = "회의실 이름", example = "회의실1")
+	private String roomName;
+	@Schema(description = "회의실 참여자 목록, 들어올 때 당시에 목록 전체를 보내줍니다.")
+	private List<UserInfoDto> userInfoList;
+	@Schema(description = "회의실 참여자 수 처음 create하면 본인만 포함돼서 1로 들어갑니다.", example = "1")
+	private Long userCount;
+}
---
+++ src/main/java/capstone/relation/websocket/meeting/dto/SdpDto.java
@@ -0,0 +1,14 @@
+package capstone.relation.websocket.meeting.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class SdpDto {
+	@Schema(description = "offer 또는 answer", example = "offer")
+	private String type;
+	@Schema(description = "sdp", example =
+		"v=0\r\no=- 123456 123456 IN IP4 127. 0. 0. 1\r\ns=-\r\nt=0 0\r\na=group:BUNDLE data\r\n"
+			+ "a=msid-semantic: WMS\r\nm=application 9 UDP/DTLS/SCTP webrtc-datachannel\r\nc=IN IP4")
+	private String sdp;
+}
