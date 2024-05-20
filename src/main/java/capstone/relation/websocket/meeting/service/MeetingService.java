d21c57e3933c42e705b0aa8eac8bf412def45aa6
hawardShin <joushin@student.42seoul.kr>
2024-05-20 14:58:00 +0900

    KAN-85: Feat: change redis Structure - 레디스 구조 변경 이유 - 워크스페이스 당 방 목록을 가져와아햠. - 유저 당 하나의 방 밖에 접속하지 못함.

--- src/main/java/capstone/relation/websocket/meeting/service/MeetingService.java
+++ src/main/java/capstone/relation/websocket/meeting/service/MeetingService.java
@@ -1,6 +1,8 @@
 package capstone.relation.websocket.meeting.service;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.springframework.data.redis.core.HashOperations;
@@ -30,18 +32,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MeetingService {
-	private static final String KEY = "MeetingRoomUsers";
+	private static final String WORK_KEY = "WORKSPACE_ROOM_PARTICIPANTS";
+	private static final String USER_KEY = "USER_ROOM_MAPPING";
 	private final UserRepository userRepository;
 	private final WorkSpaceRepository workSpaceRepository;
 	private final MeetRoomRepository meetRoomRepository;
 	private final SocketRegistry socketRegistry;
 	private final RedisTemplate<String, Object> redisTemplate;
-	private HashOperations<String, String, Set<String>> hashOperations;
 	private final SimpMessagingTemplate simpMessagingTemplate;
+	private HashOperations<String, String, HashMap<String, Set<String>>> workspaceRoomParticipants;
+	//workspaceId, roomId, userIds
+	private HashOperations<String, String, String> userRoomMapping;
 
 	@PostConstruct
 	protected void init() {
-		hashOperations = redisTemplate.opsForHash();
+		workspaceRoomParticipants = redisTemplate.opsForHash();
+		userRoomMapping = redisTemplate.opsForHash();
 	}
 
 	@Transactional(readOnly = false)
@@ -77,7 +83,7 @@ public class MeetingService {
 		meetRoomRepository.save(meetRoom);
 		workSpace.addMeetRoom(meetRoom);
 		Long roomId = meetRoom.getRoomId();
-		Set<String> userIds = addUserToRoom(roomId, userId);
+		Set<String> userIds = addUserToRoom(workSpaceId, roomId, userId);
 		Set<UserInfoDto> userInfoList = new HashSet<>();
 		for (String id : userIds) {
 			UserInfoDto userInfoDto = new UserInfoDto();
@@ -87,35 +93,51 @@ public class MeetingService {
 		return new JoinResponseDto(roomId, roomName, userInfoList, (long)userIds.size());
 	}
 
-	public Set<String> addUserToRoom(Long roomId, String userId) {
-		Set<String> userIds = hashOperations.get(KEY, roomId.toString());
-		if (userIds == null) {
-			userIds = new HashSet<>();
+	public Set<String> addUserToRoom(String workspaceId, Long roomId, String userId) {
+		HashMap<String, Set<String>> roomParticipants = workspaceRoomParticipants.get(WORK_KEY, workspaceId);
+		Set<String> userIds = new HashSet<>();
+		if (roomParticipants == null) {
+			roomParticipants = new HashMap<>();
+		} else {
+			userIds.add(userId);
 		}
-		userIds.add(userId);
-		hashOperations.put(KEY, roomId.toString(), userIds);
+		Set<String> users = roomParticipants.get(roomId.toString());
+		if (users == null) {
+			users = new HashSet<>();
+		}
+		users.add(userId);
+		roomParticipants.put(roomId.toString(), users);
+		workspaceRoomParticipants.put(WORK_KEY, workspaceId, roomParticipants);
+		userRoomMapping.put(USER_KEY, userId, roomId.toString());
 		return userIds;
 	}
 
-	public void removeUserFromRoom(Long roomId, String userId) {
-		Set<String> userIds = hashOperations.get(KEY, roomId.toString());
+	public void removeUserFromRoom(String workspaceId, Long roomId, String userId) {
+		userRoomMapping.delete(USER_KEY, userId);
+		HashMap<String, Set<String>> roomParticipants = workspaceRoomParticipants.get(WORK_KEY, workspaceId);
+		Set<String> userIds = roomParticipants.get(roomId.toString());
 		userIds.remove(userId);
-		hashOperations.put(KEY, roomId.toString(), userIds);
+		roomParticipants.put(roomId.toString(), userIds);
+		workspaceRoomParticipants.put(WORK_KEY, workspaceId, roomParticipants);
 	}
 
-	public Set<String> getRoomMembers(Long roomId) {
-		return hashOperations.get(KEY, roomId.toString());
+	public Set<String> getRoomMembers(String workspaceId, Long roomId) {
+		System.out.println("workspaceId = " + workspaceId);
+		HashMap<String, Set<String>> stringSetHashMap = workspaceRoomParticipants.get(WORK_KEY, workspaceId);
+		return stringSetHashMap.get(roomId.toString());
 	}
 
 	public MeetingRoomListDto getRoomList(String workSpaceId) {
 		// Set<String> roomList = meetingRoomRepository.getRoomList(workSpaceId);
 		Set<MeetRoom> meetRooms = meetRoomRepository.findAllByWorkSpaceId(workSpaceId);
 		MeetingRoomListDto meetingRoomListDto = new MeetingRoomListDto();
+		System.out.println("workSpaceId = " + workSpaceId);
 		for (MeetRoom meetRoom : meetRooms) {
+			System.out.println("meetRoom.getRoomId() = " + meetRoom.getRoomId());
 			MeetingRoomDto meetingRoomDto = new MeetingRoomDto();
 			meetingRoomDto.setRoomId(meetRoom.getRoomId());
 			meetingRoomDto.setRoomName(meetRoom.getRoomName());
-			meetingRoomDto.setUserCount(getRoomMembers(meetRoom.getRoomId()).size());
+			meetingRoomDto.setUserCount(getRoomMembers(workSpaceId, meetRoom.getRoomId()).size());
 			meetingRoomListDto.getMeetingRoomList().add(meetingRoomDto);
 		}
 		return meetingRoomListDto;
@@ -126,29 +148,29 @@ public class MeetingService {
 			ResponseEntity.status(status).body(message));
 	}
 
-	// public JoinResponseDto joinRoom(Map<String, Object> sessionAttributes, String roomId) {
-	// 	String userId = sessionAttributes.get("userId").toString();
-	// 	if (userId == null) { //TODO: exception
-	// 		throw new IllegalArgumentException("User does not exist: " + userId);
-	// 	}
-	// 	if (roomId == null) { //TODO: exception
-	// 		throw new IllegalArgumentException("Room does not exist: " + roomId);
-	// 	}
-	// 	if (meetingRoomRepository.isUserInRoom(roomId, userId)) {
-	// 		throw new IllegalArgumentException("User is already in the room: " + userId);
-	// 	}
-	// 	Set<String> userIds = meetingRoomRepository.enterRoom(roomId, userId);
-	// 	Set<UserInfoDto> userInfoList = new HashSet<>();
-	// 	for (String id : userIds) {
-	// 		UserInfoDto userInfoDto = new UserInfoDto();
-	// 		userRepository.findById(Long.parseLong(id)).ifPresent(userInfoDto::setByUserEntity);
-	// 		userInfoList.add(userInfoDto);
-	// 	}
-	// 	return new JoinResponseDto(roomId, meetingRoomRepository.getRoomName(roomId), userInfoList,
-	// 		(long)userIds.size());
-	// }
-	//
-	// public String getRoomName(String roomId) {
-	// 	return meetingRoomRepository.getRoomName(roomId);
-	// }
+	public JoinResponseDto joinRoom(Map<String, Object> sessionAttributes, String roomId) {
+		String userId = sessionAttributes.get("userId").toString();
+		if (userId == null) { //TODO: exception
+			throw new IllegalArgumentException("User does not exist: " + userId);
+		}
+		if (roomId == null) { //TODO: exception
+			throw new IllegalArgumentException("Room does not exist: " + roomId);
+		}
+		//유저가 이미 회의 방에 있는지 확인
+		String s = userRoomMapping.get(USER_KEY, userId);
+		if (s != null) {
+			throw new IllegalArgumentException("User is already in the room: " + userId);
+		}
+		// Set<String> userIds = meetingRoomRepository.enterRoom(roomId, userId);
+		// Set<UserInfoDto> userInfoList = new HashSet<>();
+		// for (String id : userIds) {
+		// 	UserInfoDto userInfoDto = new UserInfoDto();
+		// 	userRepository.findById(Long.parseLong(id)).ifPresent(userInfoDto::setByUserEntity);
+		// 	userInfoList.add(userInfoDto);
+		// }
+		// return new JoinResponseDto(roomId, meetingRoomRepository.getRoomName(roomId), userInfoList,
+		// 	(long)userIds.size());
+		return null;
+	}
+
 }
