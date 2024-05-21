69e698f109ebf99673829d9dba18f7c5908c9825
hawardShin <joushin@student.42seoul.kr>
2024-05-21 22:04:04 +0900

    KAN-85: Feat: SendOffer
    - sendoffer 해서 받는거 확인 후 handle 하는 중

--- src/main/resources/static/meeting/js/main.js
+++ src/main/resources/static/meeting/js/main.js
@@ -4,32 +4,24 @@ var stompClient = null;
 var JWT = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZXMiOiJVU0VSIiwiZXhwIjoxNzE4ODYxNTQ0fQ.Ast8TkAPyO2lTto253avdOFHhkiC4tjBsF33DPRv4QU";
 var WORKSPACE_ID = "f162c91d-ec28-4575-949e-9f1f65f81053";
 var roomId;
-var userId = "1";
-var isChannelReady = false;
-var isInitiator = false;
-var isStarted = false;
+
 var localStream;
 var pcs = {};
 var remoteStreams = {};
-var turnReady;
-var amIInRoom = false;
 var pcConfig = {
     'iceServers': [{
         'urls': 'stun:stun.l.google.com:19302'
     }]
 };
 
+var localVideo = document.querySelector('#localVideo');
+var remoteVideos = document.querySelector('#remoteVideo');
+
 // Set up audio and video regardless of what devices are present.
 var sdpConstraints = {
     offerToReceiveAudio: true,
     offerToReceiveVideo: true
 };
-
-/////////////////////////////////////////////
-
-var room = 'foo';
-// Could prompt for room name:
-// room = prompt('Enter room name:');
 
 
 function connect() {
@@ -60,124 +52,56 @@ function connect() {
             updateJoinMessages(JSON.parse(message.body));
             //조인 되면 offer를 보낸다.
             if (localStream) { //이전에 stream은 얻어왔다고 생각한다.
-                handleJoin(message.body);
+                handleJoin(JSON.parse(message.body));
             }
         });
         stompClient.subscribe('/topic/messages', function (messageOutput) {
             onMessageReceived(JSON.parse(messageOutput.body));
         });
-        // console.log('Connected: ' + frame);
-        // if (room !== '') {
-        //     sendMessage({room: room, type: 'create or join'});
-        //     console.log('Attempted to create or join room', room);
-        // }
     });
 }
 
+// stream을 얻어오는 함수 (내 화면 나오게 하기)
+function gotStream(stream) {
+    console.log('Adding local stream.');
+    localStream = stream;
+    localVideo.srcObject = stream;
+}
+
 function handleJoin(message) {
-    if (typeof message === 'string') {
-        try {
-            message = JSON.parse(message);
-        } catch (e) {
-            console.error('Failed to parse message:', e);
-            return;
-        }
-    }
     roomId = message.roomId;
     console.log("HANDLE JOIN!!!");
     console.log(message);
-
-    document.getElementById("roomId").innerText = message.roomId;
-    if (message.userCount === "1") {//내가 방을 만든 경우
+    document.getElementById("roomId").innerText = roomId;
+    //여기서 방해 들어왔으니 구독을 2개 해줍니다.
+    //TODO: 연결이 끊겼을 때 구독 해제 필요
+    stompClient.subscribe("/user/queue/offer/" + roomId, function (messageOutput) {
+        console.log('Offer message received:', messageOutput);
+        handleOffer(JSON.parse(messageOutput.body));
+    });
+    stompClient.subscribe("/user/queue/answer/" + roomId, function (messageOutput) {
+        console.log('Answer message received:', messageOutput);
+        handleAnswer(JSON.parse(messageOutput.body));
+    });
+    if (message.userCount === 1 || message.userCount === "1") {//내가 방을 만든 경우
         console.log('Created room ' + message.roomId);
     } else {//내가 방에 조인한 경우
         console.log('joined: ' + message["userInfoList"]);
-        //TODO: 여기서 몇 명인지 알아 내서 그만큼 화면에 띄워야해요.
-        message["userInfoList"].forEach(function (userInfo) {
+
+        //TODO: 여기서 몇 명인지 알아 내서 그만큼 화면에 띄워야해요. 이미 있던 유저 수만큼 추가되는 느낌
+        message["userInfoList"]?.forEach(function (userInfo) {
             console.log('userInfo.userId:', userInfo.userId);
             console.log(userInfo);
-            if (userInfo.userId !== userId) {
-                createPeerConnection(userInfo.userId);
+            var MyUserId = document.getElementById("userId").value;
+            if (userInfo.userId != MyUserId) {
+                createPeerConnectionAndSendOffer(userInfo.userId);
             }
         });
     }
 }
 
-// function sendMessage(message) {
-//     console.log('Client sending message: ', message);
-//     stompClient.send("/app/message", {}, JSON.stringify(message));
-// }
-
-function onMessageReceived(message) {
-    console.log('Client received message:', message);
-    if (message.type === 'got user media') {
-        maybeStart();
-    } else if (message.type === 'offer') {
-        if (!isInitiator && !isStarted) {
-            maybeStart();
-        }
-        pc.setRemoteDescription(new RTCSessionDescription(message));
-        doAnswer();
-    } else if (message.type === 'answer' && isStarted) {
-        pc.setRemoteDescription(new RTCSessionDescription(message));
-    } else if (message.type === 'candidate' && isStarted) {
-        var candidate = new RTCIceCandidate({
-            sdpMLineIndex: message.label,
-            candidate: message.candidate
-        });
-        pc.addIceCandidate(candidate);
-    } else if (message.type === 'bye' && isStarted) {
-        handleRemoteHangup();
-    }
-}
-
-var localVideo = document.querySelector('#localVideo');
-var remoteVideo = document.querySelector('#remoteVideo');
-
-
-//여기서 stream을 등록해서 저장하고 있습니다.
-function gotStream(stream) {
-    console.log('Adding local stream.');
-    localStream = stream;
-    localVideo.srcObject = stream;
-    // sendMessage({type: 'got user media'});
-    // if (isInitiator) {
-    //     maybeStart();
-    // }
-}
-
-var constraints = {
-    video: true
-};
-
-console.log('Getting user media with constraints', constraints);
-
-if (location.hostname !== 'localhost') {
-    requestTurn(
-        'https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913'
-    );
-}
-
-function maybeStart() {
-    console.log('>>>>>>> maybeStart() ', isStarted, localStream, isChannelReady);
-    if (!isStarted && typeof localStream !== 'undefined' && isChannelReady) {
-        console.log('>>>>>> creating peer connection');
-        createPeerConnection(userId);
-        pc.addStream(localStream);
-        isStarted = true;
-        console.log('isInitiator', isInitiator);
-        if (isInitiator) {
-            doCall();
-        }
-    }
-}
-
-window.onbeforeunload = function () {
-    sendMessage({type: 'bye'});
-};
-
 //이 함수에서 connection을 만들고 offer도 보낸다.
-function createPeerConnection(userId) {
+function createPeerConnectionAndSendOffer(userId) {
     try {
         var pc = new RTCPeerConnection(pcConfig);
         pc.onicecandidate = function (event) {
@@ -188,16 +112,10 @@ function createPeerConnection(userId) {
         };
         pc.onremovestream = handleRemoteStreamRemoved;
         pcs[userId] = pc;
-        console.log('Created RTCPeerConnnection for user:', userId);
-        console.log('Sending offer to peer');
         pc.addStream(localStream);
         pc.createOffer((description) => {
             setLocalAndSendOffer(description, userId)
         }, handleCreateOfferError);
-        // pc.createOffer().then(
-        //     setLocalAndSendMessage,
-        //     onCreateSessionDescriptionError
-        // );
 
     } catch (e) {
         console.log('Failed to create PeerConnection, exception: ' + e.message);
@@ -206,36 +124,26 @@ function createPeerConnection(userId) {
     }
 }
 
-function handleIceCandidate(event, userId) {
-    console.log('icecandidate event: ', event);
-    if (event.candidate) {
-        sendMessage({
-            type: 'candidate',
-            label: event.candidate.sdpMLineIndex,
-            id: event.candidate.sdpMid,
-            candidate: event.candidate.candidate,
-            userId: userId
-        });
-    } else {
-        console.log('End of candidates.');
-    }
-}
-
-function handleCreateOfferError(event) {
-    console.log('createOffer() error: ', event);
-}
-
-function doCall() {
-    console.log('Sending offer to peer');
-    // pc.createOffer(setLocalAndSendMessage, handleCreateOfferError);
-}
-
-function doAnswer() {
-    console.log('Sending answer to peer.');
-    pc.createAnswer().then(
-        setLocalAndSendMessage,
+function handleOffer(message) {
+    console.log("handleOffer");
+    console.log(message);
+    var userId = message.userId; //보낸사람의 ID
+    var sessionDescription = message.sessionDescription;
+    pcs[userId].setRemoteDescription(sessionDescription);
+    pcs[userId].createAnswer().then(
+        function (description) {
+            setLocalAndSendAnswer(description, userId)
+        },
         onCreateSessionDescriptionError
     );
+}
+
+function handleAnswer(message) {
+    console.log("handleAnswer");
+    console.log(message);
+    var userId = message.userId;
+    var sessionDescription = message.sessionDescription;
+    pcs[userId].setRemoteDescription(sessionDescription);
 }
 
 function setLocalAndSendOffer(sessionDescription, userId) {
@@ -249,6 +157,56 @@ function setLocalAndSendOffer(sessionDescription, userId) {
         userId: userId,
         sessionDescription: sessionDescription
     }));
+}
+
+var constraints = {
+    video: true
+};
+console.log('Getting user media with constraints', constraints);
+if (location.hostname !== 'localhost') {
+    requestTurn(
+        'https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913'
+    );
+}
+
+//TODO: 여기에서 연결이 끊어짐을 알려줘야 합니다. 이벤트 아직 미구현
+window.onbeforeunload = function () {
+    sendMessage({type: 'bye'});
+};
+
+
+function handleIceCandidate(event, userId) {
+    console.log('icecandidate event: ', event);
+    if (event.candidate) {
+        // sendMessage({
+        //     type: 'candidate',
+        //     label: event.candidate.sdpMLineIndex,
+        //     id: event.candidate.sdpMid,
+        //     candidate: event.candidate.candidate,
+        //     userId: userId
+        // });
+    } else {
+        console.log('End of candidates.');
+    }
+}
+
+function handleCreateOfferError(event) {
+    console.log('createOffer() error: ', event);
+}
+
+
+function setLocalAndSendAnswer(sessionDescription, userId) {
+    console.log('setLocalAndSendMessage sending message', sessionDescription);
+    console.log("userId" + userId);
+    pcs[userId].setLocalDescription(sessionDescription);
+    console.log('setLocalAndSendMessage sending message', sessionDescription);
+    //create Offer를 보낸다.
+
+    stompClient.send("/app/answer/" + roomId, {}, JSON.stringify({
+        userId: userId,
+        sessionDescription: sessionDescription
+    }));
+
 }
 
 function onCreateSessionDescriptionError(error) {
@@ -298,23 +256,11 @@ function handleRemoteStreamRemoved(event) {
     console.log('Remote stream removed. Event: ', event);
 }
 
-function hangup() {
-    console.log('Hanging up.');
-    stop();
-    sendMessage({type: 'bye'});
-}
 
-function handleRemoteHangup() {
-    console.log('Session terminated.');
-    stop();
-    isInitiator = false;
-}
+/**
+ * 이 아래 함수는 HTML에서 사용하는 함수들입니다.
+ */
 
-function stop() {
-    isStarted = false;
-    pc.close();
-    pc = null;
-}
 
 function disconnect() {
     if (stompClient !== null) {
@@ -329,8 +275,8 @@ function createRoom() {
 }
 
 function joinRoom() {
-    var roomName = prompt("Enter Room Id:");
-    stompClient.send("/app/room/join/" + roomId, {}, JSON.stringify({'roomName': roomName}));
+    var joinId = prompt("Enter Room Id:");
+    stompClient.send("/app/room/join/" + joinId, {}, JSON.stringify({}));
 }
 
 function leaveRoom() {
