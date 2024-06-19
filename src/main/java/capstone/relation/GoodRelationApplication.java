8fd354cf3ec0ffef617d1d990fb1f6e1a592d743
hawardShin <joushin@student.42seoul.kr>
2024-06-19 16:12:50 +0900

    KAN-110 : login config

--- src/main/java/capstone/relation/GoodRelationApplication.java
+++ src/main/java/capstone/relation/GoodRelationApplication.java
@@ -2,10 +2,12 @@ package capstone.relation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@ConfigurationPropertiesScan
 public class GoodRelationApplication {
 
 	public static void main(String[] args) {
