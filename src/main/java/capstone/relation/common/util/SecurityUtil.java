44ada4a9f0ff24904e9ffda6b34b7afcd1b7f607
hawardShin <joushin@student.42seoul.kr>
2024-07-20 14:03:29 +0900

    Feat: 유저 인증 로직 util class 로 분리

---
+++ src/main/java/capstone/relation/common/util/SecurityUtil.java
@@ -0,0 +1,30 @@
+package capstone.relation.common.util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import capstone.relation.api.auth.jwt.SecurityUser;
+
+public class SecurityUtil {
+
+	private SecurityUtil() {
+		throw new IllegalStateException("Utility class");
+	}
+
+	public static UserDetails getCurrentUserDetails() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+			return (UserDetails)authentication.getPrincipal();
+		}
+		throw new IllegalStateException("User not authenticated");
+	}
+
+	public static Long getCurrentUserId() {
+		UserDetails userDetails = getCurrentUserDetails();
+		if (userDetails instanceof SecurityUser) {  // Assuming SecurityUser extends UserDetails
+			return ((SecurityUser)userDetails).getUserId();
+		}
+		throw new IllegalStateException("UserDetails does not contain userId");
+	}
+}
