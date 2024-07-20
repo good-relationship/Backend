ebbea112e5ae663eeca629719a3e7a5b43e265ed
hawardShin <joushin@student.42seoul.kr>
2024-07-20 14:07:18 +0900

    Feat: Test Security context Set annotation

---
+++ src/test/java/capstone/relation/security/WithMockCustomUserSecurityContextFactory.java
@@ -0,0 +1,24 @@
+package capstone.relation.security;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+import capstone.relation.api.auth.jwt.SecurityUser;
+
+public class WithMockCustomUserSecurityContextFactory
+	implements WithSecurityContextFactory<WithMockCustomUser> {
+
+	@Override
+	public SecurityContext createSecurityContext(WithMockCustomUser customUser) {
+		System.out.println("customUser: " + customUser);
+		SecurityContext context = SecurityContextHolder.createEmptyContext();
+		SecurityUser securityUser = SecurityUser.of(customUser.id(), customUser.role());
+		System.out.println("securityUser: " + securityUser);
+		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(securityUser, null,
+			securityUser.getAuthorities());
+		context.setAuthentication(auth);
+		return context;
+	}
+}
