5545c97057630a82071408400990d913796740ef
hawardShin <joushin@student.42seoul.kr>
2024-08-28 22:03:54 +0900

    KAN-111 Feat: Create Spring Environment

---
+++ src/main/java/capstone/relation/global/helper/SpringEnvironmentHelper.java
@@ -0,0 +1,47 @@
+package capstone.relation.global.helper;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class SpringEnvironmentHelper {
+
+	private final Environment environment;
+
+	private final String PROD = "prod";
+	private final String STAGING = "staging";
+	private final String DEV = "dev";
+
+	private final List<String> PROD_AND_STAGING = List.of("staging", "prod");
+
+	public Boolean isProdProfile() {
+		String[] activeProfiles = environment.getActiveProfiles();
+		List<String> currentProfile = Arrays.stream(activeProfiles).toList();
+		return currentProfile.contains(PROD);
+	}
+
+	public Boolean isStagingProfile() {
+		String[] activeProfiles = environment.getActiveProfiles();
+		List<String> currentProfile = Arrays.stream(activeProfiles).toList();
+		return currentProfile.contains(STAGING);
+	}
+
+	public Boolean isDevProfile() {
+		String[] activeProfiles = environment.getActiveProfiles();
+		List<String> currentProfile = Arrays.stream(activeProfiles).toList();
+		return currentProfile.contains(DEV);
+	}
+
+	public Boolean isProdAndStagingProfile() {
+		String[] activeProfiles = environment.getActiveProfiles();
+		List<String> currentProfile = Arrays.stream(activeProfiles).toList();
+		return CollectionUtils.containsAny(PROD_AND_STAGING, currentProfile);
+	}
+}
