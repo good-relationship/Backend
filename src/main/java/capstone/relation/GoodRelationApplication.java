523ea501bd2e4881316687d5e84f42f82cfa3612
hawardShin <joushin@student.42seoul.kr>
2024-11-07 17:29:01 +0900

    Chore: Mongo db example delete

--- src/main/java/capstone/relation/GoodRelationApplication.java
+++ src/main/java/capstone/relation/GoodRelationApplication.java
@@ -17,24 +17,5 @@ public class GoodRelationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoodRelationApplication.class, args);
-		// String connectionString = "mongodb+srv://wnddms12345:<db_password>@clustergoodrelation.0qglt.mongodb.net/?retryWrites=true&w=majority&appName=ClusterGoodRelation";
-		// ServerApi serverApi = ServerApi.builder()
-		// 	.version(ServerApiVersion.V1)
-		// 	.build();
-		// MongoClientSettings settings = MongoClientSettings.builder()
-		// 	.applyConnectionString(new ConnectionString(connectionString))
-		// 	.serverApi(serverApi)
-		// 	.build();
-		// // Create a new client and connect to the server
-		// try (MongoClient mongoClient = MongoClients.create(settings)) {
-		// 	try {
-		// 		// Send a ping to confirm a successful connection
-		// 		MongoDatabase database = mongoClient.getDatabase("admin");
-		// 		database.runCommand(new Document("ping", 1));
-		// 		System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
-		// 	} catch (MongoException e) {
-		// 		e.printStackTrace();
-		// 	}
-		// }
 	}
 }
