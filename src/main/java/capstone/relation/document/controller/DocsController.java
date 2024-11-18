44c1612ec3f32e9c16bbae0fe058c5b6b82a8e4c
hawardShin <joushin@student.42seoul.kr>
2024-11-18 23:13:28 +0900

    Feat: Set controller create and update

--- src/main/java/capstone/relation/document/controller/DocsController.java
+++ src/main/java/capstone/relation/document/controller/DocsController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import capstone.relation.document.domain.NoteInfo;
 import capstone.relation.document.dto.CreateFileReqDto;
+import capstone.relation.document.dto.FileInfoDto;
 import capstone.relation.document.dto.FolderCreateDto;
 import capstone.relation.document.dto.FolderInfoDto;
 import capstone.relation.document.service.DocsService;
@@ -39,13 +41,20 @@ public class DocsController {
 		return docsService.getFolder(id);
 	}
 
+	@PostMapping("/file")
+	public FileInfoDto createFile(@RequestBody CreateFileReqDto createFileReqDto) {
+		return docsService.createFile(SecurityUtil.getCurrentUserId(), createFileReqDto.getFileName(), createFileReqDto.getFolderId(),createFileReqDto.getContent());
+	}
+
+	@PutMapping("/file/{id}")
+	public FileInfoDto updateFile(@PathVariable Long id, @RequestBody CreateFileReqDto createFileReqDto) {
+		return docsService.updateFile(id, createFileReqDto.getFileName(), createFileReqDto.getFolderId(), createFileReqDto.getContent());
+	}
+
 	@GetMapping("/file/{id}")
 	public NoteInfo getFile(@PathVariable String id) {
 		return docsService.getFile(id);
 	}
 
-	@PostMapping("/file")
-	public void createFile(@RequestBody CreateFileReqDto createFileReqDto) {
-		docsService.createFile(SecurityUtil.getCurrentUserId(), createFileReqDto.getFileName(), createFileReqDto.getFolderId());
-	}
+
 }
